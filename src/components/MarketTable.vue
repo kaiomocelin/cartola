f99a817ca059c9b0f862d7529dcb401951d99cf4
Kaio Mocelin <kaio.mocelin@dealerup.com.br>
2023-04-15 11:28:45 -0300

    add filter

--- src/components/MarketTable.vue
+++ src/components/MarketTable.vue
@@ -10,7 +10,26 @@
             dark
             virtual-scroll
             :rows-per-page-options="[0]"
+            :loading="loadingTableData"
+            :filter="filter"
         >
+            <template v-slot:loading>
+                <q-inner-loading showing color="primary" />
+            </template>
+            <template v-slot:top-right>
+                <q-input
+                    dark
+                    borderless
+                    dense
+                    debounce="300"
+                    v-model="filter"
+                    placeholder="Search"
+                >
+                    <template v-slot:append>
+                        <q-icon name="search" />
+                    </template>
+                </q-input>
+            </template>
             <template v-slot:body-cell-clube_id="props">
                 <q-td style="width: 20px" :props="props">
                     <img id="shield" :src="shieldTeam(props.row.clube_id)" />
@@ -41,73 +60,75 @@ export default {
             playersList: [],
             teams: [],
             positions: [],
+            loadingTableData: true,
+            filter: "",
             columns: [
                 {
                     name: "clube_id",
                     label: "Time",
                     align: "left",
-                    style:'width: 20px',
+                    style: "width: 20px",
                     field: (row) => row.clube_id,
                     // format: (val) => `${val}`,
-                    sortable: true,
+                    sortable: true
                 },
                 {
                     name: "status_id",
                     align: "left",
                     label: "Status",
                     field: "status_id",
-                    sortable: true,
+                    sortable: true
                 },
                 {
                     name: "apelido",
                     align: "left",
-                    style:'width: 240px',
+                    style: "width: 240px",
                     label: "Jogador",
                     field: "apelido",
-                    sortable: true,
+                    sortable: true
                 },
                 {
                     name: "posicao_id",
                     align: "left",
                     label: "Posição",
                     field: "posicao_id",
-                    sortable: true,
+                    sortable: true
                 },
                 {
                     name: "jogos_num",
                     align: "right",
                     label: "Jogos",
                     field: "jogos_num",
-                    sortable: true,
+                    sortable: true
                 },
                 {
                     name: "preco_num",
                     align: "right",
                     label: "Preço",
                     field: "preco_num",
-                    sortable: true,
+                    sortable: true
                 },
                 {
                     name: "pontos_num",
                     align: "right",
                     label: "Última",
                     field: "pontos_num",
-                    sortable: true,
+                    sortable: true
                 },
                 {
                     name: "media_num",
                     align: "right",
                     label: "Média",
                     field: "media_num",
-                    sortable: true,
+                    sortable: true
                 },
                 {
                     name: "minimo_para_valorizar",
                     label: "Mín. p/ Valorizar",
                     field: "minimo_para_valorizar",
-                    sortable: true,
-                },
-            ],
+                    sortable: true
+                }
+            ]
         };
     },
 
@@ -117,9 +138,9 @@ export default {
                 { value: 0, text: "Todas" },
                 ...Object.entries(this.positions).map((i) => {
                     return { value: i[1].id, text: i[1].nome };
-                }),
+                })
             ];
-        },
+        }
     },
 
     mounted() {
@@ -128,6 +149,7 @@ export default {
 
     methods: {
         load() {
+            this.loadingTableData = true;
             axios
                 .get("https://api.cartola.globo.com/atletas/mercado")
                 .then((res) => {
@@ -137,6 +159,9 @@ export default {
                     this.playersList = this.players;
                     this.teams = res.data.clubes;
                     this.positions = res.data.posicoes;
+                })
+                .finally(() => {
+                    this.loadingTableData = false;
                 });
         },
 
@@ -149,8 +174,8 @@ export default {
         shieldTeam(code) {
             let teams = Object.entries(this.teams);
             return teams.filter((i) => i[0] == code)[0][1].escudos["30x30"];
-        },
-    },
+        }
+    }
 };
 </script>
 
